10e3d3f152a8db5730ec85141dc26ef587dbeee5
Mads Harby <harbymads@gmail.com>
2022-05-11 12:15:24 +0200

    Did more stuff

--- src/Components/CreateTicketEmbed.js
+++ src/Components/CreateTicketEmbed.js
@@ -1,93 +1,148 @@
-function CreateTicketEmbed() {
-  const doCreateTicket = () => {
-    console.log("BOOBA");
+import React, { Component } from "react";
+import axios from "axios";
+//import CreateTicket from "../Models/CreateTicket";
+class CreateTicketEmbed extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      subject: "",
+      message: "",
+      email: "",
+      firstName: "",
+      lastName: "",
+      phoneNumber: "",
+    };
+  }
+
+  submitHandler = (e) => {
+    e.preventDefault();
+    console.log(this.state);
+    axios
+      .post("http://vps.qvistgaard.me:8980/api/ticket", this.state)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
 
-  return (
-    <>
-      <h1>Create a ticket</h1>
-      <p>
-        Here you are able to create a support ticket manually, in case of an
-        user calling on the phone etc.
-      </p>
+  changeHandler = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
 
-      <div className="row">
-        <div className="col-12 col-md-6">
-          <h4>User Information</h4>
-          <div className="input-group mb-3">
-            <span className="input-group-text sb-form-text">First Name</span>
-            <input
-              id="firstName"
-              type="text"
-              className="form-control"
-              placeholder="First Name..."
-            />
+  render() {
+    const { subject, message, email, firstName, lastName, phoneNumber } =
+      this.state;
+
+    return (
+      <>
+        <h1>Create a ticket</h1>
+        <p>
+          Here you are able to create a support ticket manually, in case of an
+          user calling on the phone etc.
+        </p>
+
+        <form onSubmit={this.submitHandler}>
+          <div className="row">
+            <div className="col-12 col-md-6">
+              <h4>User Information</h4>
+              <div className="input-group mb-3">
+                <span className="input-group-text sb-form-text">
+                  First Name
+                </span>
+                <input
+                  id="firstName"
+                  name="firstName"
+                  value={firstName}
+                  onChange={this.changeHandler}
+                  type="text"
+                  className="form-control"
+                  placeholder="First Name..."
+                />
+              </div>
+              <div className="input-group mb-3">
+                <span className="input-group-text sb-form-text">Last Name</span>
+                <input
+                  id="lastName"
+                  name="lastName"
+                  value={lastName}
+                  onChange={this.changeHandler}
+                  type="text"
+                  className="form-control"
+                  placeholder="Last Name..."
+                />
+              </div>
+              <div className="input-group mb-3">
+                <span className="input-group-text sb-form-text">E-mail</span>
+                <input
+                  id="email"
+                  name="email"
+                  value={email}
+                  onChange={this.changeHandler}
+                  type="text"
+                  className="form-control"
+                  placeholder="E-mail..."
+                />
+              </div>
+              <div className="input-group mb-3">
+                <span className="input-group-text sb-form-text">
+                  Phone Number
+                </span>
+                <input
+                  id="phoneNumber"
+                  name="phoneNumber"
+                  value={phoneNumber}
+                  onChange={this.changeHandler}
+                  type="tel"
+                  className="form-control"
+                  placeholder="Phone number..."
+                />
+              </div>
+            </div>
+            <div className="col-12 col-md-6">
+              <h4>Ticket Content</h4>
+              <div className="input-group mb-3">
+                <span className="input-group-text sb-form-text">Subject</span>
+                <input
+                  id="subject"
+                  name="subject"
+                  value={subject}
+                  onChange={this.changeHandler}
+                  type="text"
+                  className="form-control"
+                  placeholder="Ticket subject..."
+                />
+              </div>
+              <label htmlFor="message" className="form-label">
+                Message:
+              </label>
+              <textarea
+                id="message"
+                name="message"
+                value={message}
+                onChange={this.changeHandler}
+                className="form-control mb-3"
+                placeholder="Ticket message..."
+                rows="4"
+              ></textarea>
+            </div>
           </div>
-          <div className="input-group mb-3">
-            <span className="input-group-text sb-form-text">Last Name</span>
-            <input
-              id="lastName"
-              type="text"
-              className="form-control"
-              placeholder="Last Name..."
-            />
+          <div className="row">
+            <div className="col-12 col-md-6">
+              <button type="submit" className="btn btn-primary w-100 mb-3">
+                Submit
+              </button>
+            </div>
+            <div className="col-12 col-md-6">
+              <button className="btn btn-danger w-100 mb-3">Reset</button>
+            </div>
           </div>
-          <div className="input-group mb-3">
-            <span className="input-group-text sb-form-text">E-mail</span>
-            <input
-              id="email"
-              type="text"
-              className="form-control"
-              placeholder="E-mail..."
-            />
-          </div>
-          <div className="input-group mb-3">
-            <span className="input-group-text sb-form-text">Phone Number</span>
-            <input
-              id="phoneNumber"
-              type="tel"
-              className="form-control"
-              placeholder="Phone number..."
-            />
-          </div>
-        </div>
-        <div className="col-12 col-md-6">
-          <h4>Ticket Content</h4>
-          <div className="input-group mb-3">
-            <span className="input-group-text sb-form-text">Subject</span>
-            <input
-              id="subject"
-              type="text"
-              className="form-control"
-              placeholder="Ticket subject..."
-            />
-          </div>
-          <label htmlFor="message" className="form-label">
-            Message:
-          </label>
-          <textarea
-            id="message"
-            className="form-control mb-3"
-            placeholder="Ticket message..."
-            rows="4"
-          ></textarea>
-        </div>
-      </div>
-      <div className="row">
-        <div className="col-12 col-md-6">
-          <button
-            onClick={doCreateTicket}
-            className="btn btn-primary w-100 mb-3"
-          >
-            Submit
-          </button>
-        </div>
-        <div className="col-12 col-md-6">
-          <button className="btn btn-danger w-100 mb-3">Reset</button>
-        </div>
-      </div>
-    </>
-  );
+        </form>
+      </>
+    );
+  }
 }
 
 export default CreateTicketEmbed;
