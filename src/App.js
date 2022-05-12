b738814a656439775a6d03d10439e98cf6bbb390
Mads Harby <harbymads@gmail.com>
2022-05-12 12:21:15 +0200

    asdas

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import CreateTicketEmbed from "./Components/CreateTicketEmbed";
 import "bootstrap/dist/css/bootstrap.min.css";
+import LookAtTicket from "./Components/LookAtTicket";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Router>
         <Routes>
           <Route exact path="/" element={<CreateTicketEmbed />}></Route>
+          <Route exact path="/LookAtTicket" element={<LookAtTicket />}></Route>
         </Routes>
       </Router>
     </>
