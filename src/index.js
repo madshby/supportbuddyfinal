4054038b56d34c5f247c43a669987205361178e5
Mads Harby <harbymads@gmail.com>
2022-05-18 11:01:02 +0200

    Implemented Live Chat Avatar

--- src/index.js
+++ src/index.js
@@ -2,6 +2,29 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import Avatar from "./Components/Avatar";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+const AvatarWidget = () => {
+  return (
+    <>
+      <div>Avatar Widget</div>
+      <div>
+        <Avatar
+          style={{
+            position: "fixed",
+            bottom: "24px",
+            right: "24px",
+          }}
+        />
+      </div>
+    </>
+  );
+};
+
+root.render(
+  <>
+    <App />
+    <AvatarWidget></AvatarWidget>
+  </>
+);
